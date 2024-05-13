858552dcee09ede16e6e97143da94a339b1e4612
Hannes Pétur Eggertsson <hannespetur@gmail.com>
2024-05-13 21:31:36 +0000

    cleanup

--- ruvnaveidar.py
+++ ruvnaveidar.py
@@ -5,18 +5,18 @@ import json
 from ruvnaveidar_utils import *
 import os
 
-## Main
-if __name__ == "__main__":
+if __name__ == '__main__':
   parser = argparse.ArgumentParser()
 
-  parser.add_argument("-o", "--output", help = "output_directory.", default = "out")
-  parser.add_argument('-l', '--list', help = "Only list all RÚV programs.", action='store_true')
+  parser.add_argument('-o', '--output', help = 'Output directory for downloads.', default = 'out')
+  parser.add_argument('-l', '--filelist', help = 'Only list all RÚV programs and write to file.', default = '')
+  parser.add_argument('-v', '--verbose', action = 'store_true')
 
   args = parser.parse_args()
 
-  api_data = fetch_programs(is_updated=True)
-  whitelist = list(read_file_list("whitelist.txt"))
-  blacklist = read_file_list("blacklist.txt")
+  api_data = fetch_programs()
+  whitelist = list(read_file_list('whitelist.txt'))
+  blacklist = read_file_list('blacklist.txt')
   seen_pids = set() # Note which programs have been checked
 
   if not os.path.exists(args.output):
@@ -24,59 +24,64 @@ if __name__ == "__main__":
 
   for panel in api_data:
     # panel["format"] is either "tv" or "radio". All radio streams are ignored
-    if panel["format"] != "tv":
+    if panel['format'] != 'tv':
       continue
 
-    pid = panel["id"]
+    pid = panel['id']
 
     if pid in seen_pids:
       continue
 
     seen_pids.add(pid)
 
-    if args.list:
-      print(panel["id"], panel["title"])
+    if len(args.filelist) > 0:
+      with open(args.filelist, 'r', encoding='utf-8') as f:
+        print(panel['id'], panel['title'])
       continue
 
-    p_title = trim_season_number_suffix(panel["title"])
+    p_title = trim_season_number_suffix(panel['title'])
 
     if not is_title_in_list(whitelist, p_title):
       continue
 
-    ruv_api_program = "https://api.ruv.is/api/programs/program/{}/all".format(pid)
+    if args.verbose:
+      print('#Matched "{}"'.format(p_title))
+    ruv_api_program = 'https://api.ruv.is/api/programs/program/{}/all'.format(pid)
     p_r = get_url(ruv_api_program)
 
-    if str(p_r) == "<Response [404]>":
-      ruv_api_program2 = "https://api.ruv.is/api/programs/get_ids/{}".format(pid)
+    if str(p_r) == '<Response [404]>':
+      ruv_api_program2 = 'https://api.ruv.is/api/programs/get_ids/{}'.format(pid)
       p_r = get_url(ruv_api_program2)
-      if str(p_r) == "<Response [404]>":
+      if str(p_r) == '<Response [404]>':
         continue
 
     try:
       p_data = p_r.json()
     except:
-      print("ERROR: Failed reading JSON data from RÚV for pid={}".format(pid))
+      print('ERROR: Failed reading JSON data from RÚV for pid={}'.format(pid))
       raise
 
-    p_data = p_data["programs"] if "programs" in p_data else p_data
+    p_data = p_data['programs'] if 'programs' in p_data else p_data
     p_data = p_data if isinstance(p_data, list) else [p_data]
 
     for program in p_data:
-      season_num = get_season_number_suffix(panel["title"])
+      season_num = get_season_number_suffix(panel['title'])
 
-      if "foreign_title" in program and len(program["foreign_title"]) > 0:
-        title = "{}".format(trim_season_number_suffix(program["foreign_title"]))
+      if "foreign_title" in program and len(program['foreign_title']) > 0:
+        title = trim_season_number_suffix(program['foreign_title'])
       else:
         title = p_title
 
-      for episode in program["episodes"]:
-        eid = episode["id"]
-        full_id = "{}|{}".format(pid, eid)
+      for episode in program['episodes']:
+        eid = episode['id']
+        full_id = '{}|{}'.format(pid, eid)
 
         if full_id in blacklist:
           continue
 
-        ep_num = str(episode["number"]).zfill(2)
-        out_fn = args.output + "/" + title
-        out_fn += " - S{}E{} - {}.ts".format(season_num, ep_num, episode["title"])
-        print('streamlink --output "{}" {} best && echo "{}|{}" >> blacklist.txt'.format(out_fn, episode["file"], pid, eid))
+        ep_num = str(episode['number']).zfill(2)
+        out_fn = args.output + '/' + title
+        out_fn += ' - S{}E{} - {}.ts'.format(season_num, ep_num, episode['title'])
+        cmd = 'streamlink --output "{}" {} best'.format(out_fn, episode['file'])
+        cmd +=  ' && echo "{}|{}" >> blacklist.txt'.format(pid, eid)
+        print(cmd)
--- ruvnaveidar_utils.py
+++ ruvnaveidar_utils.py
@@ -34,24 +34,24 @@ def get_url(url, retries=5):
   return session.get(url)
 
 
-assert(is_roman_numeral("Blæja") == False)
-assert(is_roman_numeral("XI") == True)
-assert(is_roman_numeral("I") == True)
-assert(is_roman_numeral("1") == False)
+assert(is_roman_numeral('Blæja') == False)
+assert(is_roman_numeral('XI') == True)
+assert(is_roman_numeral('I') == True)
+assert(is_roman_numeral('1') == False)
 
 
 # Removes any season number related suffixes for a given series title
 # Ex. Monsurnar 1 => Monsurnar
 #     Hvolpasveitin IV => Hvolpasveitin
 def trim_season_number_suffix(title):
-  title_words = title.split(" ")
+  title_words = title.split(' ')
   if len(title_words) >= 2 and (title_words[-1].isdigit() or is_roman_numeral(title_words[-1])):
-    return " ".join(title_words[0:-1])
+    return ' '.join(title_words[0:-1])
   else:
     return title
 
 def get_season_number_suffix(title):
-  title_words = title.split(" ")
+  title_words = title.split(' ')
 
   if len(title_words) >= 2:
     if title_words[-1].isdigit():
@@ -63,7 +63,7 @@ def get_season_number_suffix(title):
     except:
       pass
 
-  return "99"
+  return '99'
 
 
 ##
@@ -81,44 +81,37 @@ def read_file_list(fn):
   s = set()
 
   if os.path.exists(fn):
-    with open(fn, "r") as f:
+    with open(fn, 'r', encoding='utf-8') as f:
       for line in f:
-        if not line.startswith("#"):
+        if not line.startswith('#'):
           s.add(line.lower().rstrip())
 
   return s
 
 ## Fetch all RÚV programs
-def fetch_programs(is_updated = True):
+def fetch_programs():
   ruv_api_url_all = 'https://api.ruv.is/api/programs/all'
   ruv_api_url_featured = 'https://api.ruv.is/api/programs/featured/tv'
   api_data = None
 
-  if is_updated or not os.path.exists("data_list.json"):
-    r = get_url(ruv_api_url_all)
+  r = get_url(ruv_api_url_all)
 
-    try:
-      api_data = r.json()
-    except:
-      print("Failed reading JSON data from RÚV for all programs (old API)")
-      raise
+  try:
+    api_data = r.json()
+  except:
+    print('ERROR: Failed reading JSON data from RÚV for all programs (old API)')
+    raise
 
-    r = get_url(ruv_api_url_featured)
+  r = get_url(ruv_api_url_featured)
 
-    try:
-      api_data2 = r.json()["panels"]
-    except:
-      print("Failed reading JSON data from RÚV for all programs (new API)")
-      raise
+  try:
+    api_data2 = r.json()['panels']
+  except:
+    print('ERROR: Failed reading JSON data from RÚV for all programs (new API)')
+    raise
 
-    for panel_data in api_data2:
-      if 'programs' in panel_data:
-        api_data.extend(panel_data['programs'])
-
-    with open("data.json", "w") as f:
-      json.dump(api_data, f)
-  else:
-    f = open("data.json")
-    api_data = json.load(f)
+  for panel_data in api_data2:
+    if 'programs' in panel_data:
+      api_data.extend(panel_data['programs'])
 
   return api_data
